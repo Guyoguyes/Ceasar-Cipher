9983324d0e9713c81fe3f502457f5782832e767c
Guyoguyes <g.abuba43@gmail.com>
2020-05-22 15:31:24 +0300

    second test for encryption

--- src/main/java/Encryption.java
+++ src/main/java/Encryption.java
@@ -1,2 +1,26 @@
 public class Encryption {
+    private String encryptWord = "";
+    private int shiftKey;
+    private String errorMessage;
+
+
+    public String Encryption(String text, int shiftKey){
+       if(shiftKey < 0 || shiftKey > 26){
+           String errorM = "Error Key must be between 0 and 26";
+
+       }
+
+
+        return encryptWord;
+    }
+
+    public String getEncryptWord(){
+        return encryptWord
+    }
+
+    public String getErrorMessage(){
+        return errorMessage;
+    }
+
+
 }
--- src/test/java/EncryptionTest.java
+++ src/test/java/EncryptionTest.java
@@ -1,5 +1,21 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class EncryptionTest {
+
+    @Test
+    public void encrpyt_returnsEncryptedWord_String(){
+        Encryption testEncryption = new Encryption();
+        String  expected = "JK";
+        assertEquals(expected, testEncryption.Encryption("HI", 2));
+    }
+
+    @Test
+    public void encrpyt_returnsShiftNumberOrError_true(){
+        Encryption testEncryption = new Encryption();
+        boolean expectedOutPut = false;
+        assertEquals(expectedOutPut, testEncryption.getErrorMessage());
+    }
 
 }
