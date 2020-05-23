3710f6fbb5c6057077e1103385ec0fc91fb6735d
Guyoguyes <g.abuba43@gmail.com>
2020-05-23 06:05:06 +0300

    Changed input reader from Scanner to BufferReader

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,16 +1,22 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws IOException {
-        Scanner scanner = new Scanner(System.in);
+//        Scanner scanner = new Scanner(System.in);
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("------------Caeser Cipher ----------------");
 
         System.out.println("Enter what you would like to encrypt");
-        String encryptWord = scanner.next();
+        String encryptWord = bufferedReader.readLine();
         System.out.println("Enter Key");
-        Integer encryptionKey = scanner.nextInt();
+        String stringEncryptionKey = bufferedReader.readLine();
+        int encryptionKey = Integer.parseInt(stringEncryptionKey);
         Encryption encryption = new Encryption(encryptWord, encryptionKey);
+        String cipher = encryption.getEncryptWord();
         System.out.println("Your Encrypted word is ");
         System.out.println(encryption);
     }
